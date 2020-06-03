2e61757648647446ed23316cc903fae5d43741ea
=KSM <sminggo5@naver.com>
2020-06-03 17:31:20 +0900

    Update ranking update logic back

--- [05] 소스코드/backend/src/main/java/com/roomoftruth/rot/repository/AgentRepository.java
+++ [05] 소스코드/backend/src/main/java/com/roomoftruth/rot/repository/AgentRepository.java
@@ -2,14 +2,19 @@ package com.roomoftruth.rot.repository;
 
 import com.roomoftruth.rot.domain.Agent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AgentRepository extends JpaRepository<Agent, Long>{
 
-//    @Query(nativeQuery = true, value = )
-//    select u.num, u.nickname as name, a.name as agentName, u.picture as uPicture, a.picture as aPicture, a.point, rank() over (order by point desc) as rnk
-//    from user u, agent a
-//    where u.num = a.num
-//    order by
-//    point desc limit 9
-//    List<Agent> findAgent();
+//    List<Agent> findTop9AgentOrderByPointDesc();
+    @Query(nativeQuery = true, value = "SELECT agent_id, user_num, name, picture, point, rank() over (order by point desc) as rnk, " +
+            "created_at, updated_at, address, count, license, representative FROM agent ORDER BY point DESC")
+    List<Agent> getRankingTop9();
+
+    @Modifying
+    @Query(value = "update agent set rnk = ?1 where agent_id = ?2", nativeQuery = true)
+    void updateRank(int rnk, long id);
 }
