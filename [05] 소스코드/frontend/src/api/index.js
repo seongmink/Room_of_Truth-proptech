733a4ee4402f5b04c31ebe24854b124be264d6dd
Jang Woo Young <tmlu48@naver.com>
2020-06-09 02:58:43 +0900

    Fix userapi

--- [05] 소스코드/frontend/src/api/index.js
+++ [05] 소스코드/frontend/src/api/index.js
@@ -3,10 +3,10 @@ import axios from "axios";
 function createInstance() {
   const instance = axios.create({
     //baseURL: "http://211.46.225.84:8080/",
-   //baseURL: "https://k02b2031.p.ssafy.io",
+   baseURL: "https://k02b2031.p.ssafy.io",
     //baseURL: "http://192.168.43.241:8080/",
    // baseURL: "http://172.20.10.11:8080",
-   baseURL: "http://192.168.0.18:8080", //홍주
+   //baseURL: "http://192.168.0.18:8080", //홍주
     headers: {
       "Content-Type": "application/json"
     }
