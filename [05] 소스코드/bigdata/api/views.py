44944096aaba641e92f194704406445d1a75b7b5
hongju <95hongju@gmail.com>
2020-06-08 22:57:20 +0900

    Fix views(add function : search around

--- [05] 소스코드/bigdata/api/views.py
+++ [05] 소스코드/bigdata/api/views.py
@@ -47,6 +47,27 @@ class Contract(APIView, PaginationHandlerMixin):
         return Response(serializer.data, status=status.HTTP_200_OK)
         
 
+class Around(APIView, PaginationHandlerMixin):
+    pagination_class = SmallPagination
+    serializer_class = serializers.AroundSerializer
+
+    def get(self, request, format=None, *args, **kwargs):
+        keyword = request.query_params.get("keyword",None)
+      
+        if keyword is None:
+            instance = models.Around.objects.all()
+        else:
+            instance = models.Around.objects.filter(address__contains=keyword)
+
+        page = self.paginate_queryset(instance)
+        if page is not None:
+            serializer = self.get_paginated_response(self.serializer_class(page,
+ many=True).data)
+        else:
+            serializer = self.serializer_class(instance, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+        
+
 class Favorites(APIView, PaginationHandlerMixin):
     pagination_class = SmallPagination
     serializer_class = serializers.FavoriteSerializer
@@ -75,14 +96,14 @@ class Favorites(APIView, PaginationHandlerMixin):
 class ContractChart(APIView):
 
     def getAgg(self, instance_addr, instance_emd, target,key):
-        label = {'2019-04':0,'2019-05':1,'2019-06':2,'2019-07':3,'2019-08':4,'2019-09':5,'2019-10':6,'2019-11':7,'2019-12':8,'2020-01':9,'2020-02':10,'2020-03':11,'2020-04':12}
+        label = {'2019-04':0,'2019-05':1,'2019-06':2,'2019-07':3,'2019-08':4,'2019-09':5,'2019-10':6,'2019-11':7,'2019-12':8,'2020-01':9,'2020-02':10,'2020-03':11,'2020-04':12,'2020-05':13,'2020-06':14}
         # --
         result_addr = (instance_addr.filter(detail=key)
         .values_list('contract_date__year', 'contract_date__month','detail')
         .annotate(Avg(target))
         .order_by('contract_date__year', 'contract_date__month'))
         # print(result_addr)
-        addrs = [None]*13
+        addrs = [None]*15
         for r in result_addr:
             m=str(r[1])
             if r[1]<10:
@@ -95,7 +116,7 @@ class ContractChart(APIView):
         .annotate(Avg(target))
         .order_by('contract_date__year', 'contract_date__month'))
         # print(result_emd)
-        emds = [None] * 13
+        emds = [None] * 15
         for r in result_emd:
             m=str(r[1])
             if r[1]<10:
@@ -116,7 +137,10 @@ class ContractChart(APIView):
         if detail is None:
             return Response("detail을 mm(매매),js(전세),ws(월세) 중 하나를 입력해주세요",status=status.HTTP_400_BAD_REQUEST)
         if address is not None and detail is not None: # 둘다 있는 값이라면
+            # print(address)
             bd = models.Contract.objects.filter(address=address).first()
+            # print(bd)
+            # print('-----------')
             sd = bd.sd
             sgg = bd.sgg # 시군구
             emd = bd.emd # 읍면동
@@ -135,7 +159,7 @@ class ContractChart(APIView):
                     return_value = {
                         'road_address':address,
                         'dong_address':calc_address,
-                        'label' : ['2019-04','2019-05','2019-06','2019-07','2019-08','2019-09','2019-10','2019-11','2019-12','2020-01','2020-02','2020-03','2020-04'],
+                        'label' : ['2019-04','2019-05','2019-06','2019-07','2019-08','2019-09','2019-10','2019-11','2019-12','2020-01','2020-02','2020-03','2020-04','2020-05','2020-06'],
                         'datatype': key,
                         'dong_data' :emds,
                         'addr_data':addrs,
@@ -146,7 +170,7 @@ class ContractChart(APIView):
                     return_value = {
                         'road_address':address,
                         'dong_address':calc_address,
-                        'label' : ['2019-04','2019-05','2019-06','2019-07','2019-08','2019-09','2019-10','2019-11','2019-12','2020-01','2020-02','2020-03','2020-04'],
+                        'label' : ['2019-04','2019-05','2019-06','2019-07','2019-08','2019-09','2019-10','2019-11','2019-12','2020-01','2020-02','2020-03','2020-04','2020-05','2020-06'],
                         'datatype': key,
                         'dong_data' :emds,
                         'addr_data':addrs
