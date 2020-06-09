843b5b140140fb672b6c58cf074762bd53fc9e03
Jang Woo Young <tmlu48@naver.com>
2020-06-09 13:10:14 +0900

    Fix api

--- [05] 소스코드/frontend/src/api/index.js
+++ [05] 소스코드/frontend/src/api/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 function createInstance() {
   const instance = axios.create({
     //baseURL: "http://211.46.225.84:8080/",
-   baseURL: "http://k02b2031.p.ssafy.io",
+  baseURL: "http://k02b2031.p.ssafy.io",
     //baseURL: "http://192.168.43.241:8080/",
    // baseURL: "http://172.20.10.11:8080",
    //baseURL: "http://192.168.0.18:8080", //홍주
@@ -16,7 +16,7 @@ function createInstance() {
 
 function getUrl() {
   //return "http://192.168.43.241:8080/"
-   return "https://k02b2031.p.ssafy.io/images/"
+   return "http://k02b2031.p.ssafy.io/images/"
 }
 
 export { createInstance, getUrl };
